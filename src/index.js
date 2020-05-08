abf171e96c4706e6d26f7f81481adb5bc203b003
rainyulei <leiyu19880522@icloud.com>
2020-05-08 18:49:45 +1200

    improvement(message.js src/index.js message.css): message component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
-import React, {Component} from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MessageComponent from './message';
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+class Msg {
+  constructor() {
+    let myRef = { current: '' };
+    const div = document.createElement('div');
+    document.body.append(div);
+    ReactDOM.render(<MessageComponent ref={myRef} />, div);
+    this.refs = myRef;
+  }
+  success(options) {
+    this.refs.current.add(options);
   }
 }
+
+Msg.getInstance = (function () {
+  let instance;
+  return function () {
+    if (!instance) {
+      instance = new Msg();
+    }
+    return instance;
+  };
+})();
+
+export const Message = Msg.getInstance();
